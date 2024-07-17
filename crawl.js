203c1f19c84b6019e6c00370b7b49ebcbaac177b
hemantkumbhar10 <hemantkumbhar010@gmail.com>
2024-07-17 05:41:07 +0530

    getURLsFrom function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,31 @@
+const { JSDOM } = require("jsdom");
+
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll("a");
+  for (const link of linkElements) {
+    if (link.href.slice(0, 1) === "/") {
+      //relative urls
+      try {
+        const urlObj = new URL(`${baseURL}${link.href}`);
+        urls.push(urlObj.href);
+      } catch (e) {
+        console.log("Error with relative URL: ", e.message);
+      }
+    } else {
+      //absolute urls
+      try {
+        const urlObj = new URL(link.href);
+        urls.push(urlObj.href);
+      } catch (e) {
+        console.log("Error with absolute URL: ", e.message);
+      }
+    }
+  }
+  return urls;
+}
+
 function normalizeURL(urlString) {
   const urlObj = new URL(urlString);
 
@@ -10,4 +38,4 @@ function normalizeURL(urlString) {
   return hostpath;
 }
 
-module.exports = { normalizeURL };
+module.exports = { getURLsFromHTML, normalizeURL };
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require("./crawl.js");
+const { normalizeURL, getURLsFromHTML } = require("./crawl.js");
 const { test, expect } = require("@jest/globals");
 
 test("normalizeURL strip protocol", () => {
@@ -26,5 +26,69 @@ test("normalizeURL strip http", () => {
   const input = "http://dummyjson.com/recipes";
   const actual = normalizeURL(input);
   const expected = "dummyjson.com/recipes";
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML absolute", () => {
+  const inputHTMLBody = `
+<html>
+    <body>
+        <a href="https://dummyjson.com/recipes/">Dummy Json Recipes</a>
+    </body>
+</html>
+`;
+
+  const inputBaseURL = "https://dummyjson.com/recipes/";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://dummyjson.com/recipes/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML relative", () => {
+  const inputHTMLBody = `
+<html>
+    <body>
+        <a href="/recipes/">Dummy Json Recipes</a>
+    </body>
+</html>
+`;
+
+  const inputBaseURL = "https://dummyjson.com";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://dummyjson.com/recipes/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML both relative and absolute", () => {
+  const inputHTMLBody = `
+<html>
+    <body>
+        <a href="https://dummyjson.com/carts/">Dummy Json Absolute Carts</a>
+        <a href="/recipes/">Dummy Json Relative Recipes</a>
+    </body>
+</html>
+`;
+
+  const inputBaseURL = "https://dummyjson.com";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = [
+    "https://dummyjson.com/carts/",
+    "https://dummyjson.com/recipes/",
+  ];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML invalid", () => {
+  const inputHTMLBody = `
+<html>
+    <body>
+        <a href="invalid">Invalid Link</a>
+    </body>
+</html>
+`;
+
+  const inputBaseURL = "https://dummyjson.com";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = [];
   expect(actual).toEqual(expected);
 });
