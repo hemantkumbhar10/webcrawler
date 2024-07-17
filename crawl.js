a9ef04b0c51b3df17869ad76d5e8e01d02404277
hemantkumbhar10 <hemantkumbhar010@gmail.com>
2024-07-18 00:05:33 +0530

    Crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,32 @@
 const { JSDOM } = require("jsdom");
 
+async function crawlPage(currentURL) {
+  console.log(`Actively crawling: ${currentURL}`);
+
+  try {
+    const response = await fetch(currentURL);
+
+    if (response.status > 399) {
+      console.log(
+        `Error in fetch with status code: ${response.status} on page ${currentURL}`,
+      );
+      return;
+    }
+
+    const contentType = response.headers.get("content-type");
+    if (!contentType.includes("text/html")) {
+      console.log(
+        `Non HTML response, content type: ${contentType}, on page: ${currentURL}`,
+      );
+      return;
+    }
+
+    console.log(await response.text());
+  } catch (e) {
+    console.log(`Error in fetch. ${e.message}, on page ${currentURL}`);
+  }
+}
+
 function getURLsFromHTML(htmlBody, baseURL) {
   const urls = [];
   const dom = new JSDOM(htmlBody);
@@ -38,4 +65,4 @@ function normalizeURL(urlString) {
   return hostpath;
 }
 
-module.exports = { getURLsFromHTML, normalizeURL };
+module.exports = { crawlPage, getURLsFromHTML, normalizeURL };
--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello world");
+const { crawlPage } = require("./crawl.js");
+
+function index() {
+  if (process.argv.length < 3) {
+    console.log("No website provided!");
+    process.exit(1);
+  }
+
+  if (process.argv.length > 3) {
+    console.log("Too many command line args!");
+    process.exit(1);
+  }
+
+  // for (const arg of process.argv) {
+  //   console.log(arg);
+  //}
+
+  const baseURL = process.argv[2];
+
+  console.log(`Starting to crawl ${baseURL}`);
+  crawlPage(baseURL);
+}
+
+index();
